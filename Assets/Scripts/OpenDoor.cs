816798b534ab775ffc500cccf706b97c4ae88484
Tim Greller <timlg07@gmail.com>
2023-10-27 19:31:21 +0200

    open door script changes and resets the tile map

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using CreativeSpore.RpgMapEditor;
 using DG.Tweening;
+using UnityEditor;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    private int oldTileId = -1;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +25,18 @@ public class OpenDoor : MonoBehaviour
     public void Open()
     {
         transform.DOScaleY(0, 0.5f);
+        var tile = RpgMapHelper.GetAutoTileByPosition(transform.position, 1);
+        oldTileId = tile.Id;
+        RpgMapHelper.SetAutoTileByPosition(transform.position, 0, 1);
+    }
+
+    private void OnApplicationQuit()
+    {
+#if UNITY_EDITOR
+        if (Application.isPlaying && oldTileId != -1)
+        {
+            RpgMapHelper.SetAutoTileByPosition(transform.position, oldTileId, 1);
+        }
+#endif
     }
 }
