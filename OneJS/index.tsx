602f6db03fa3326a166633975371e5b3c5e51408
Tim Greller <timlg07@gmail.com>
2024-03-07 01:53:44 +0100

    render board, handle change on click, check for solution

--- OneJS/index.tsx
+++ OneJS/index.tsx
@@ -1,11 +1,87 @@
 import { useEventfulState } from "onejs"
+import { emo } from "onejs/styled"
 import { h, render } from "preact"
 import { useEffect, useState } from "preact/hooks"
 const doorManager = require("doorManager")
 
 
+const puzzleData = {
+    1: {
+        rows: 3,
+        cols: 3,
+        board: [
+            [1, 1, 2],
+            [2, 3, 1],
+            [3, 1, 2]
+        ],
+        orientation: [
+            [2, 2, 2],
+            [1, 1, 2],
+            [3, 2, 1]
+        ],
+        solution: [
+            [1, 0, 0],
+            [2, 1, 0],
+            [0, 1, 2]
+        ]
+    },
+}
+
 const Puzzle = ({ roomId, puzzleSolved }: { roomId: number, puzzleSolved: () => void }) => {
-    return <div class="w-full h-full flex justify-center items-center text-[#305fbc] bg-stone-600" onClick={puzzleSolved}>Room: {roomId}</div>
+    const currentPuzzle = puzzleData[roomId]
+    const [board, setBoard] = useState(currentPuzzle.orientation)
+
+    function checkRotation() {
+        for (let i = 0; i < currentPuzzle.rows; i++) {
+            for (let j = 0; j < currentPuzzle.cols; j++) {
+                const solution = currentPuzzle.solution[i][j]
+                if (solution !== 0 && solution !== board[i][j]) {
+                    return
+                }
+            }
+        }
+        puzzleSolved()
+    }
+
+    useEffect(() => {
+        checkRotation()
+    }, [board])
+
+    return <div class="w-full h-full flex justify-center items-center">
+        <div class={emo`
+            margin: 16px 32px;
+            background-color: #aaa;
+            padding: 32px;
+            width: 100%;
+            height: 100%;
+            max-width: 600px;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: stretch;
+        `}>
+                {board.map((row, i) => <div class={emo`
+                    flex: 1;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: stretch;
+                `}>
+                    {row.map((cell, j) => <div class={emo`
+                        flex: 1;
+                        border: 1px solid #000;
+                        background-color: #fff;
+                        margin: 4px;
+                    `} onClick={() => {
+                        setBoard((prevBoard) => {
+                            const newBoard = prevBoard.map(row => row.slice())
+                            newBoard[i][j] = (newBoard[i][j] % 4) + 1
+                            return newBoard
+                        })
+                    }}>{cell}</div>)}
+                </div>)}
+        </div>
+        
+        Room: {roomId}</div>
 }
 
 const App = () => {
