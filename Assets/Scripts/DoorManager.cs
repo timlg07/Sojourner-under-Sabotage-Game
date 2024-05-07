8b122fc64f33a8391a32920caa5b020658c71a8c
Tim Greller <timlg07@gmail.com>
2024-05-07 19:21:02 +0200

    allow multiple doors per room

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -7,19 +7,26 @@ using UnityEngine.Events;
 public class DoorManager : MonoBehaviour
 {
     public event Action<int> OnTryUnlockRoom; // used to trigger OneJS
-    private Dictionary<int, Door> _doors = new();
+    private Dictionary<int, List<Door>> _doors = new();
 
     public void EnableDoor(GameProgressState state)
     {
         if (state.status == GameProgressState.Status.DOOR)
         {
-            _doors[state.room].Enable();
+            UnlockAllPreviousDoors(state.room);
+            _doors[state.room].ForEach(door => door.Enable());
+        } 
+        else
+        {
+            UnlockAllPreviousDoors(state.room + 1);
         }
     }
     
     public void Register(int roomId, Door door)
     {
-        _doors.Add(roomId, door);
+        var doors = _doors.GetValueOrDefault(roomId, new List<Door>());
+        doors.Add(door);
+        _doors[roomId] = doors;
     }
 
     public void TryUnlockRoom(int roomId)
@@ -30,9 +37,24 @@ public class DoorManager : MonoBehaviour
     // called from OneJS after mini-game completed
     public void UnlockRoom(int roomId)
     {
-        if (_doors.TryGetValue(roomId, out var door))
+        if (_doors.TryGetValue(roomId, out var doors))
         {
-            door.Unlock();
+            doors.ForEach(door => door.Unlock());
+        }
+    }
+    
+    private void UnlockAllPreviousDoors(int roomId)
+    {
+        foreach (var doors in _doors)
+        {
+            if (doors.Key < roomId)
+            {
+                doors.Value.ForEach(door =>
+                {
+                    door.Unlock();
+                    door.Close();
+                });
+            }
         }
     }
 }
